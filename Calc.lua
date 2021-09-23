4ff607fe150a25a95d32f594922e8f69d4e07435
x21785 <91121035+x21785@users.noreply.github.com>
2021-09-23 14:47:41 +0530

    Add files via upload

---
+++ Calc.lua
@@ -0,0 +1,13 @@
+io.write("Simple Calculator")
+while (True) do
+local x = io.read("Num1:")
+local y = io.read("Num2:")
+local zent = io.read("Num3:")
+print("results:")
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(zennt'')
+end
